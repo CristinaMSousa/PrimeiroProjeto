a5d3f255d0230636c76ee122f98a479599977aca
CristinaMSousa <15962301-CristinaMSousa@users.noreply.replit.com>
2023-05-10 19:07:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello World, que bom estar aqui, iniciando na programação, realizando o primeiro projeto em Java."); 
+    {
+      System.out.println("Aprendendo a codar, no curso Cod3r Udemy");
+      
+    }
+    
+                      
+  
+    
+  }
+}
